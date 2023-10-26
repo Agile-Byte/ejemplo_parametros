5af6e2602ba2dc9ee804c911805e5d4a32faba35
AgileByte <118997051+Agile-Byte@users.noreply.github.com>
2023-10-26 23:50:46 +0200

    Update funciones.py

--- ejemplo_parametros/src/proyecto/funciones.py
+++ ejemplo_parametros/src/proyecto/funciones.py
@@ -5,10 +5,13 @@ from datetime import datetime
 
 def calculadora_precio(cantidad, prc_unitario):
     df_params = pd.read_excel(io=r'/home/gondaviza/Escritorio/agilebyte/ejemplo_parametros/ejemplo_parametros/src/extras/ejemplo_parametros.xlsx')
-    today = datetime.now().strftime('%A')
-
-    df_params = df_params[df_params["dia"]==today][['coef_ajuste']].reset_index(drop=True)
-    coef = df_params["coef_ajuste"].values[0]
+    day_of_week = datetime.now().strftime('%A')
+    today = datetime.now()
+    df_params_filtered = df_params[(df_params["dia"]==day_of_week) &
+                                   (today >= df_params["dat_alta"]) &
+                                   (today <= df_params["dat_baja"])
+    ][['coef_ajuste']].reset_index(drop=True)
+    coef = df_params_filtered["coef_ajuste"].values[0]
     resultado = cantidad * prc_unitario * coef
     return resultado
 
